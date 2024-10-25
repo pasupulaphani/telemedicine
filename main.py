869dee6a022dd65e275d65b1d247d21b4f29a087
new_work <new_work@new-works-Laptop.local>
2024-10-25 15:59:41 +0100

    add main file

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import aivm_client as aic
+import torch
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# List all supported models
+available_models = aic.get_supported_models()
+print(available_models)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+trans = transforms.Compose(
+    [
+        transforms.ToTensor(),                # Transform to tensor
+        transforms.Resize((28, 28)),          # Resize to (28 x 28)
+        transforms.Normalize((0.5,), (1.0,)), # Normalize the image
+    ]
+)
+# Load the dataset
+dataset = dset.MNIST(
+    root="/tmp/mnist", train=True, transform=trans, download=True
+)
+
+# Get entry #20 of the dataset
+inputs, _ =  dataset[20]
+inputs = inputs.reshape(1, 1, 28, 28)
+
+encrypted_input = aic.LeNet5Cryptensor(inputs)
+
+
+result = aic.get_prediction(encrypted_input, "LeNet5MNIST")
+
+print(result)
